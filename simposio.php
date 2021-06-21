a11d2eec34851a322e31c37f5b8fc4958201febd
Abi Rojas <abimaelromo@outlook.es>
2021-06-21 17:07:14 -0500

    agregue simposio y video

---
+++ simposio.php
@@ -0,0 +1,24 @@
+<?php
+include("header.php");
+?>
+<style>
+    centro {
+        text-align:center;
+        height:400px;
+    width:400px;
+     margin:50px auto;
+    }
+
+</style>
+<center>
+<img src="img/cn1.PNG" alt="Emblema" style="">
+
+<div style="height:600px; width:600px;">
+<iframe width="675" height="506" src="https://www.youtube.com/embed/frwCr9YJx2s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+ </div>                   
+			
+</center>
+<?php
+include("footer.php");
+?>
+
